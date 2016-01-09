9697cfabdc5ac43c5aa3801558a837f1a330ea08
John Harvey <thegradientvector@gmail.com>
2016-01-09 12:04:43 -0600

    Updated and simplified setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-pushd ".." > /dev/null
+# find directories of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "${DIR}/.." > /dev/null
 src=`pwd -P`
 popd > /dev/null
-files=".bashrc .bash_logout .profile .gitconfig .tmux.conf .vimrc .gvimrc "
 
+files=".gitconfig .tmux.conf .vimrc .gvimrc "
 for i in $files;
 do
     ln -i -s -T $src/$i ~/$i
 done
-
 ln -i -s -T $src/.gitignore.global ~/.gitignore
-ln -i -s -T $src/.vim/ ~/.vim
 
-## uncomment if you want to put your settings in a .bashrc_pre
-#echo '# use settings from my personal .bashrc' > /tmp/.bashrc
-#echo 'source ~/.bashrc_pre' >> /tmp/.bashrc
-#echo ' ' >> /tmp/.bashrc
-#cat ~/.bashrc >> /tmp/.bashrc
-#mv -i /tmp/.bashrc ~/.bashrc
+# append the contents of these files
+cat $src/.append_bashrc >> ~/.bashrc
+cat $src/.append_bash_logout >> ~/.bash_logout
+
+# copy directories
+ln -i -s -T $src/.vim/ ~/.vim
 
